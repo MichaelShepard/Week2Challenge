c8f0b161140f98caf8fe98fce9b2d10c5033c0e6
Michael Shepard <mike@rockcastlepartners.com>
2020-11-21 14:38:59 -0500

    first commit

---
+++ Week2Challenges/Greeter.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Week2Challenges
+{
+    public class Greeter
+    {
+
+        public string Name { get; set; }
+
+        public void getName ()
+        {
+            Console.WriteLine("Hello " + Name);
+        }
+
+        public void farewell ()
+        {
+            Console.WriteLine("Farewell " + Name);
+
+        }
+
+
+        public DateTime Date { get; set; }
+
+        public void timeOfDay ()
+        {
+
+            int hour = Date.Hour;
+            
+            if (hour < 12)
+            
+            {
+                Console.WriteLine("Good Morning");
+            
+            } else if (hour >= 12 || hour < 6 )
+            
+            {
+                Console.WriteLine("Good Afternoon");
+            
+            } else
+            
+            {
+                Console.WriteLine("Good Evening");
+            
+             }
+            
+
+        }
+        
+    }
+}
---
+++ Week2Challenges/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Week2Challenges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Greeter greeter = new Greeter();
+            greeter.Name = "Mike";
+
+            greeter.getName();
+            greeter.farewell();
+           
+            greeter.Date = new DateTime(1974, 7, 10, 7, 10, 24);
+            greeter.timeOfDay();
+        }
+    }
+}
